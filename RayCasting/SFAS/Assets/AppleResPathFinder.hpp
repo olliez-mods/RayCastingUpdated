656e4f13f124dc37cfda8f54cd476d3e213c35a9
olliez-mods <75761250+olliez-mods@users.noreply.github.com>
2023-02-24 16:35:30 +1000

    Delete RayCasting directory

--- RayCasting/SFAS/Assets/AppleResPathFinder.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef RESOURCE_PATH_HPP
-#define RESOURCE_PATH_HPP
-#include <string>
-namespace applerp__
-{
-std::string resourcePath(void);
-}
-
-#endif
--- RayCasting/SFAS/dt.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef dt_hpp
-#define dt_hpp
-#include <SFML/Graphics.hpp>
-
-double resetDT();
-double getDT();
-
-#endif
